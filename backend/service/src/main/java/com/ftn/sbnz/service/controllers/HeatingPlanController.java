c1b9c886490721a09f85fc4ab8ee06a0e644baf5
darkokos <darkosvilar02@gmail.com>
2025-10-03 21:54:52 +0200

    [add] controller advice for error handling

--- backend/service/src/main/java/com/ftn/sbnz/service/controllers/HeatingPlanController.java
+++ backend/service/src/main/java/com/ftn/sbnz/service/controllers/HeatingPlanController.java
@@ -2,6 +2,7 @@ package com.ftn.sbnz.service.controllers;
 
 import com.ftn.sbnz.service.dtos.HeatingGoalDto;
 import com.ftn.sbnz.service.dtos.HeatingPlanDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/heating")
 public class HeatingPlanController {
     @PostMapping
-    public HeatingGoalDto postHeatingPlan(@RequestBody HeatingPlanDto heatingPlanDto) {
+    public ResponseEntity<HeatingGoalDto> postHeatingPlan(@RequestBody HeatingPlanDto heatingPlanDto) {
         return null;
     }
 }
---
+++ backend/service/src/main/java/com/ftn/sbnz/service/dtos/ErrorMessageDto.java
@@ -0,0 +1,12 @@
+package com.ftn.sbnz.service.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ErrorMessageDto {
+    private String message;
+}
