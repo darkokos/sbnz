4cc247d69897edcf585209d3d39b3b7b66de2e8d
darkokos <darkosvilar02@gmail.com>
2025-10-03 02:22:09 +0200

    init

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { roboto } from "@/theme/fonts";
+import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/theme/theme";
+import { CssBaseline } from "@mui/material";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
