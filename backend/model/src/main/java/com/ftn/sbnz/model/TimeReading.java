b18af95d5f59f9840950a9e66acc7a36f2c23944
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-10-05 17:41:51 +0200

    feat: default state

--- backend/model/src/main/java/com/ftn/sbnz/model/TimeReading.java
+++ backend/model/src/main/java/com/ftn/sbnz/model/TimeReading.java
@@ -1,10 +1,12 @@
 package com.ftn.sbnz.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class TimeReading {
     // 0-23
     private int hour;
